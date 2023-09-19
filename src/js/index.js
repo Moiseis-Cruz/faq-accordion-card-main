41331b1889d40d29aa8540c9b4945e4d741de945
Moiseis Cruz <moizzito@gmail.com>
2023-09-19 12:38:43 -0300

    Removi redundância do código js.

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,9 @@ const item = document.querySelectorAll(".item")
 item.forEach((itemSelected) => {
     itemSelected.addEventListener('click', () => {
         const itemAtual = document.querySelector(".clicked")
-
-        if(itemAtual){
-            itemAtual.classList.remove("clicked")
-        }
         
         itemSelected.classList.add("clicked")
-
+        
         if(itemAtual){
             itemAtual.classList.remove("clicked")
         }
